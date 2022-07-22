ce80171c25f12df8e60ca94ebdb0f0a446794600
gcm <1453400328@qq.com>
2022-07-22 17:48:21 +0800

    项目配置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
+let a;
 new Vue({
   render: h => h(App),
 }).$mount('#app')
