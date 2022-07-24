0120cfb0cd4bf8cdf044e00c65c11d910ec268b0
gcm <1453400328@qq.com>
2022-07-24 16:30:39 +0800

    nprogress进度条

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,5 +1,11 @@
 //对axios进行二次封装
 import axios from "axios";
+// 引入进度条
+import nprogress from "nprogress";
+// console.log(nprogress);
+// start:进度条开始 done:进度条结束
+// 引入进度条样式
+import "nprogress/nprogress.css"
 
 const requests = axios.create({
 
@@ -14,8 +20,8 @@ const requests = axios.create({
 // 请求拦截器：再发送请求之前，请求拦截器可以检测到，可以在发送请求之前做一些事情
 requests.interceptors.request.use(config => {
 
-   
-    
+    // 进度条开始
+    nprogress.start();
     // config:配置对象，对象里面有一个属性很重要，headers请求头
     return config;
 })
@@ -23,6 +29,8 @@ requests.interceptors.request.use(config => {
 // 响应拦截器
 requests.interceptors.response.use(res => {
     
+    // 进度条结束
+    nprogress.done();
     return res.data;
 
 }, err => {
