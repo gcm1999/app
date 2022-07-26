a8a36b06b818c18c260b07f8d6d25c15642925e8
gcm <1453400328@qq.com>
2022-07-26 15:42:04 +0800

    mockjs模拟数据

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 //三级联动组件---全局组件
-import TypeNav from '@/components/TypeNav';
+import TypeNav from "@/components/TypeNav";
 // 第一个参数:全局组件的名字 第二个参数:哪一个组件
-Vue.component(TypeNav.name,TypeNav)
-
+Vue.component(TypeNav.name, TypeNav);
+// 引入mockSerce.js---mock数据
+import "@/mock/mockServe";
 
 //引入路由
-import router from '@/router';
+import router from "@/router";
 // 引入仓库
-import store from '@/store'
+import store from "@/store";
 // // 测试
 // import { reqCategoryList } from '@/api';
 // reqCategoryList();
-
 
 // Vue.config.productionTip = false
 
 let a;
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 
   //注册路由,KV一致省略V
   //注册路由信息：当这里书写router的时候，组件身上都拥有$route,$router属性
@@ -28,5 +28,4 @@ new Vue({
   router,
   // 注册仓库:组件身上会多一个属性$store
   store,
-
-}).$mount('#app')
+}).$mount("#app");
