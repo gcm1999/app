e2c921757036e3bc2bf259e5b989bfc3023ee867
gcm <1453400328@qq.com>
2022-07-26 17:51:27 +0800

    swiper实现轮播图，watch + nextTick监听数据更新

--- src/pages/Home/ListContainer/index.vue
+++ src/pages/Home/ListContainer/index.vue
@@ -6,8 +6,9 @@
         <!--banner轮播-->
         <div class="swiper-container" id="mySwiper">
           <div class="swiper-wrapper">
-            <div class="swiper-slide">
-              <img src="./images/banner1.jpg" />
+            <div class="swiper-slide" v-for="(carouse,index) in mockBannerList" :key="carouse.id">
+              <!-- <img src="./images/banner1.jpg" /> -->
+              <img :src="carouse.imgUrl" />
             </div>
             <!-- <div class="swiper-slide">
                                 <img src="./images/banner2.jpg" />
@@ -102,16 +103,53 @@
 <script>
 import { mapState } from "vuex";
 
+// 引入swiper包
+import Swiper from "swiper"
 export default {
   name: "ListContainer",
   mounted() {
     this.$store.dispatch("mockBannerList");
+    
   },
   computed: {
     ...mapState({
       mockBannerList: state=>state.home.mockBannerList,
     }),
+
   },
+  watch: {
+    
+    mockBannerList: {
+      // // 监视mockBannerList属性发生变化，但不能保证dom已经遍历完毕
+      handler(newValue, oldValue) {
+
+        // 
+        this.$nextTick(() => {
+          var mySwiper = new Swiper(".swiper-container", {
+        // direction: "vertical", // 垂直切换选项
+        loop: true, // 循环模式选项
+
+        // 如果需要分页器
+        pagination: {
+          el: ".swiper-pagination",
+          clickable:true,
+        },
+
+        // 如果需要前进后退按钮
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+
+        // 如果需要滚动条
+        scrollbar: {
+          el: ".swiper-scrollbar",
+        },
+      });
+        })
+      }
+    }
+  }
 };
 </script>
 <style scoped>
