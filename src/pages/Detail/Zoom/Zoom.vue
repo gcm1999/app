2889941df149ef5ea0b61db9a9e5bfb5f758cf3b
gcm <1453400328@qq.com>
2022-08-04 17:11:52 +0800

    放大镜

--- src/pages/Detail/Zoom/Zoom.vue
+++ src/pages/Detail/Zoom/Zoom.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="spec-preview">
     <img :src="imgObj.imgUrl" />
-    <div class="event"></div>
+    <div class="event" @mousemove="handler"></div>
     <div class="big">
-      <img :src="imgObj.imgUrl" />
+      <img :src="imgObj.imgUrl" ref="big" />
     </div>
-    <div class="mask"></div>
+    <div class="mask" ref="mask"></div>
   </div>
 </template>
 
@@ -26,6 +26,25 @@ export default {
   computed: {
     imgObj() {
       return this.skuImageList[this.currentIndex] || [];
+    },
+  },
+  methods: {
+    handler(e) {
+      let mask = this.$refs.mask;
+      let big = this.$refs.big;
+
+      let left = e.offsetX - mask.offsetWidth / 2;
+      let top = e.offsetY - mask.offsetHeight / 2;
+
+      left < 0 ? (left = 0) : (left = left);
+      left > mask.offsetWidth ? (left = mask.offsetWidth) : (left = left);
+      top < 0 ? (top = 0) : (top = top);
+      top > mask.offsetHeight ? (top = mask.offsetHeight) : (top = top);
+
+      mask.style.left = left + "px";
+      mask.style.top = top + "px";
+      big.style.left = -2 * left + "px";
+      big.style.top = -2 * top + "px";
     },
   },
 };
